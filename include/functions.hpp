92e9e10ce1e98a9055daaf11910024c0f3558328
Arsham2002 <arshammasoudi10@gamil.com>
2023-02-05 21:54:33 +0330

    fix  something in menu

--- include/functions.hpp
+++ include/functions.hpp
@@ -6,11 +6,11 @@
 void print_menu()//Prints menu and commands
 {
     cout << "Exit: exit \nAdd: add matrix 'name' 'row' 'col' You can even enter di";
-    cout << "rectly like this--> \n [a,b,c,...] (if matrix is square just enter ro";
+    cout << "rectly like this--> \n[a,b,c,...] (if matrix is square just enter ro";
     cout << "w) \nDiagonal: is_diagonal 'name' \nUpper/Lower trangular: is_upper/l";
     cout << "ower_trangular 'name' \nTrangular: is_trangular 'name' \nIdentity: is";
     cout << "_identity 'name' \nNormal/Skew symmetric: is_normal/skew_symmetric 'n";
-    cout << "ame' \nSymmetric: is_symmetric 'name' \n Inverse: inverse 'name' or i";
+    cout << "ame' \nSymmetric: is_symmetric 'name' \nInverse: inverse 'name' or i";
     cout << "nverse 'name' 'name of matrix 2' \nShow: show 'name' \nDelete: delete";
     cout << " 'name' \nChange an element: change 'name' 'row' 'column' 'new elemen";
     cout << "t \n";
